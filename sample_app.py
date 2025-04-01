522ae194a473cff0edf6995815693a2931351841
Jonathan <jonathan@example.com>
2025-04-01 06:40:13 +0000

    Prueba de ejecución automática del pipeline CI/CD

--- sample_app.py
+++ sample_app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html", ip=request.remote_addr)
+    return f"Hola, ahora con CI/CD funcionando: {request.remote_addr}\n"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5050)
